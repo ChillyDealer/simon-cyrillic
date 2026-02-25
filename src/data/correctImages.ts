82f79d3019e1461c65ef08cecaa377f8b1f78116
ChillyDealer <gregslasauri@gmail.com>
2026-02-25 13:11:03 +0100

    viewport fixes

--- src/data/correctImages.ts
+++ src/data/correctImages.ts
@@ -1,6 +1,9 @@
 export const correctImages: string[] = [
   "https://preview.redd.it/ohio-impressed-v0-oc72kazv77ig1.png?width=1170&format=png&auto=webp&s=b35a32e666a9e084cf3ecbd0ad50875fc12ddfcd",
   "https://i.ytimg.com/vi/4MbT9crhTwE/maxresdefault.jpg",
+  "https://media.tenor.com/2zeEpwGZAQ4AAAAe/spongebob-driving.png",
+  "https://i.pinimg.com/736x/fa/95/a5/fa95a5adba0b448a4866d3c78598ed9f.jpg",
+  "https://media.tenor.com/LEIWAEsO53IAAAAe/meme.png",
 ];
 
 export function pickRandomImage(): string {
